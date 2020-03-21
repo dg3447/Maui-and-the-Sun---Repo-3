d2f4cca42fe4a6fecb94fa141f58a4db55f3e63f
Bishesh Sunam <bishesh.sunam@gmail.com>
2020-03-21 17:36:51 +1300

    latest UI updates with recovered file

---
+++ maui-and-the-sun/Assets/Project- Bishesh/Scripts/TimerUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class TimerUI : MonoBehaviour
+{
+    int countdownStartValue = 5;
+    public Text timerUI;
+    [SerializeField]
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdownTimer();
+       
+    }
+
+    public void countdownTimer()
+    {
+        if (countdownStartValue>0)
+        {
+            TimeSpan spanTime = TimeSpan.FromSeconds(countdownStartValue);
+            timerUI.text = spanTime.Minutes + ":" + spanTime.Seconds;
+            countdownStartValue--;
+            Invoke("countdownTimer", 1.0f);
+        }
+        else
+        {
+
+            timerUI.text = "Time up!";
+            endgame();
+           // SceneManager.LoadScene(0);
+           
+        }
+    }
+
+    private void endgame()
+    {
+       // gameOverUI.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ maui-and-the-sun/Assets/Project- Bishesh/Scripts/gameOverMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class gameOverMenu : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("application quite!");
+        Application.Quit();
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
