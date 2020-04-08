a1b6ccaad9542d4c23454e7b1213c5fc6afa198c
Bishesh Sunam <bishesh.sunam@gmail.com>
2020-04-09 01:18:03 +1200

    item dragable and snap back into it's original slot in inventory but not dropable on target slot

---
+++ maui-and-the-sun/Assets/Project- Bishesh/Scripts/Inventory/hangi/Drag_Item.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag_Item : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Vector3 startPosition;
+    
+   
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+       
+        rectTransform.SetAsLastSibling();
+       
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+       rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = startPosition;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+       
+    }
+}
---
+++ maui-and-the-sun/Assets/Project- Bishesh/Scripts/Inventory/hangi/Drop_Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drop_Item : MonoBehaviour, IDragHandler
+{
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drop received");
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
