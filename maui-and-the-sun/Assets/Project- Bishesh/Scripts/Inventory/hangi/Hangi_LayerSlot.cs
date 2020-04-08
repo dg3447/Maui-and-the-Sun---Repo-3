320665f9a3107b9c8e5277f57d14c7639b6810db
Bishesh Sunam <bishesh.sunam@gmail.com>
2020-04-09 11:19:45 +1200

    removable inventory for testing, dragging items is completed

--- maui-and-the-sun/Assets/Project- Bishesh/Scripts/Inventory/hangi/Hangi_LayerSlot.cs
+++ maui-and-the-sun/Assets/Project- Bishesh/Scripts/Inventory/hangi/Hangi_LayerSlot.cs
@@ -1,13 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Hangi_LayerSlot : MonoBehaviour, IDropHandler
 {
+
+    public event EventHandler <OnItemDroppedEventArgs> OnItemDropped;
+
+    public class OnItemDroppedEventArgs: EventArgs
+    {
+        public items items;
+    }
+   
     public void OnDrop(PointerEventData eventData)
     {
        items items = UI_ItemDrag.Instance.GetItem();
+        OnItemDropped?.Invoke(this, new OnItemDroppedEventArgs {items = items });
+
+
         Debug.Log(items);
+
     }
 }
+
---
+++ maui-and-the-sun/Assets/Project- Bishesh/Scripts/Inventory/hangi/Hangi_layerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hangi_layerController : MonoBehaviour
+{
+    private Hangi_LayerSlot soil;
+    private Hangi_LayerSlot leaves;
+    private Hangi_LayerSlot meat;
+    private Hangi_LayerSlot vege1;
+    private Hangi_LayerSlot vege2;
+    private Hangi_LayerSlot branches;
+    private Hangi_LayerSlot smallStones;
+    private Hangi_LayerSlot largeStones;
+
+
+    private void Awake()
+    {
+        soil = transform.Find("soil").GetComponent<Hangi_LayerSlot>();
+        leaves = transform.Find("leaves").GetComponent<Hangi_LayerSlot>();
+        meat = transform.Find("meat").GetComponent<Hangi_LayerSlot>();
+        vege1 = transform.Find("vege1").GetComponent<Hangi_LayerSlot>();
+        vege2 = transform.Find("vege2").GetComponent<Hangi_LayerSlot>();
+        branches = transform.Find("branches").GetComponent<Hangi_LayerSlot>();
+        smallStones = transform.Find("smallStones").GetComponent<Hangi_LayerSlot>();
+        largeStones = transform.Find("largeStones").GetComponent<Hangi_LayerSlot>();
+
+        soil.OnItemDropped += Soil_OnItemDropped;
+    }
+
+    private void Soil_OnItemDropped(object sender, Hangi_LayerSlot.OnItemDroppedEventArgs e)
+    {
+        Debug.Log("item dropped is:" + e.items);
+    }
+}
